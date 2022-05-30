f3272f03a104eeaef2f457997ccb5ef12ed59994
Mila2k2 <ludmila.n.anjos@gmail.com>
2022-05-29 23:06:49 -0300

    teste-pot-funcionando-mesmo

--- Arduino_Desafio3/Arduino_Desafio3.ino
+++ Arduino_Desafio3/Arduino_Desafio3.ino
@@ -1,13 +1,14 @@
-#define pot1 A0
+const int pot1 = A5;
 
-int valor_pot = 0;
+float valor_pot = 0;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(pot1, INPUT);
 }
 
 void loop() {
-  valor_pot = analogRead(pot1);
-  delay(10);
+  valor_pot = map(analogRead(pot1),0,1023,0,255);
+  delay(100);
   Serial.println(valor_pot);
 }
