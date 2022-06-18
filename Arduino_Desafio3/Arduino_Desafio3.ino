72c51629f7808b9083a18ab9abb83ee8b80cf73d
Ludmila Nascimento Dos Anjos <94915125+Mila2k2@users.noreply.github.com>
2022-06-18 17:00:55 -0300

    enviando pacote pela serial

--- Arduino_Desafio3/Arduino_Desafio3.ino
+++ Arduino_Desafio3/Arduino_Desafio3.ino
@@ -1,5 +1,5 @@
 const int pot1 = A5;
-const int pot2 = A4;
+const int pot2 = A3;
 
 const int b01 = 9;
 const int b02 = 8;
@@ -14,36 +14,34 @@ bool v_b02;
 bool v_b03;
 bool v_b04;
 
+int arr[10];
+
 void setup() {
   Serial.begin(9600);
   pinMode(pot1, INPUT);
+  pinMode(pot2, INPUT);
+  
   for(int i = 6; i <= 9; i++){
-      pinMode(i, INPUT);
+      pinMode(i, INPUT_PULLUP);
     }
 }
 
 void loop() {
   valor_pot1 = map(analogRead(pot1),0,1023,0,255);
-  Serial.print("Potenciometro 1: ");
-  Serial.println(valor_pot1);
+  Serial.print(String(valor_pot1) + " - ");
   
   valor_pot2 = map(analogRead(pot2),0,1023,0,255);
-  Serial.print("Potenciometro 2: ");
-  Serial.println(valor_pot2);
+  Serial.print(String(valor_pot2) + " - ");
   
-  v_b01 = digitalRead(b01);
-  v_b02 = digitalRead(b02);
+  v_b01 = !digitalRead(b01);
+  v_b02 = !digitalRead(b02);
 
-  v_b03 = digitalRead(b03);
-  v_b04 = digitalRead(b04);
-  
-  delay(800);
-  Serial.print("Botao 1: ");
-  Serial.println(v_b01);
-  Serial.print("Botao 2: ");
-  Serial.println(v_b02);
-  Serial.print("Botao 3: ");
-  Serial.println(v_b03);
-  Serial.print("Botao 4: ");
-  Serial.println(v_b04);
+  v_b03 = !digitalRead(b03);
+  v_b04 = !digitalRead(b04);
+
+  Serial.print(String(v_b01) + " - ");
+  Serial.print(String(v_b02) + " - ");
+  Serial.print(String(v_b03) + " - ");
+  Serial.print(String(v_b04) + "\n");
+  delay(500);
 }
